7308a8563a8e13aee8711c0854015e990f5cdeb5
Brenda Martins <brendamichele16@hotmail.com>
2022-11-30 07:46:19 -0300

    Resolved ValidPalindrome problem

--- org.leetcode/src/main/java/org/leetcode/weekOne/ValidPalindrome.java
+++ org.leetcode/src/main/java/org/leetcode/weekOne/ValidPalindrome.java
@@ -1,15 +1,32 @@
 package org.leetcode.weekOne;
 
+import java.util.Stack;
+
 public class ValidPalindrome {
 
     public static void main(String[] args) {
 
-        System.out.println("It is a palindrome: " + isPalindrome("aça"));
+        System.out.println("It is a palindrome: " + isPalindrome("ada"));
     }
 
     public static boolean isPalindrome(String s) {
+        Stack<Character> stack = new Stack<>();
 
+        for (int i = 0; i <  s.length(); i++) {
 
-        return true;
+            stack.push(s.charAt(i));
+        }
+
+        String palindrome = "";
+
+        while (!stack.isEmpty()){
+            palindrome += stack.pop();
+        }
+
+        if(palindrome.equalsIgnoreCase(s)){
+            return true;
+        }
+
+        return false;
     }
 }
