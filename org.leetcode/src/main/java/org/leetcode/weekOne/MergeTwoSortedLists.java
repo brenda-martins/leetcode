11b5fde0b5313b12704cee24579fb0f9e5fcc102
Brenda Martins <brenda.martins-ext@ab-inbev.com>
2022-11-29 21:00:26 -0300

    Implementing new solution for 'Merge two sorted list' problem

--- org.leetcode/src/main/java/org/leetcode/weekOne/MergeTwoSortedLists.java
+++ org.leetcode/src/main/java/org/leetcode/weekOne/MergeTwoSortedLists.java
@@ -7,11 +7,20 @@ public class MergeTwoSortedLists {
         ListNode list1 = new ListNode(1, new ListNode(2, new ListNode(4)));
         ListNode list2 = new ListNode(1, new ListNode(3, new ListNode(4)));
 
-        ListNode listNode = mergeTwoLists(list1, list2);
+         var listNode = mergeTwoLists(list1, list2);
 
+        System.out.println("Solution One");
         while(listNode.next != null) {
             System.out.println(listNode.val);
             listNode = listNode.next;
+        }
+
+        var listNodeSolutionTwo = mergeTwoListsSolutionTwo(list1, list2);
+
+        System.out.println("Solution two");
+        while(listNodeSolutionTwo.next != null) {
+            System.out.println(listNodeSolutionTwo.val);
+            listNodeSolutionTwo = listNodeSolutionTwo.next;
         }
     }
 
@@ -40,6 +49,48 @@ public class MergeTwoSortedLists {
         }
 
          return dummy.next;
+    }
+
+    private static ListNode mergeTwoListsSolutionTwo(ListNode list1, ListNode list2){
+        if(list1 == null) return list2;
+        if(list2 == null) return list1;
+
+        var restNode = new ListNode();
+
+        if(list1.val > list2.val){
+            restNode.val = list2.val;
+            list2 = list2.next;
+        }else{
+            restNode.val = list1.val;
+            list1 = list1.next;
+        }
+
+        var lastNode = restNode;
+
+        while(list1 != null && list2 != null){
+
+            var nowNode = new ListNode();
+
+            if(list1.val > list2.val){
+                nowNode.val = list2.val;
+                list2 = list2.next;
+            }else{
+                nowNode.val = list1.val;
+                list1 = list1.next;
+
+            }
+
+            lastNode.next = nowNode;
+            lastNode = nowNode;
+        }
+
+        if(list1 != null){
+            lastNode.next = list1;
+        }else{
+            lastNode.next = list2;
+        }
+
+        return  restNode;
     }
 
 }
