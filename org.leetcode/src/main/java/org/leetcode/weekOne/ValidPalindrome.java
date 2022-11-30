d486b45d2186eb087bd5427058f936f7ad36f5e2
Brenda Martins <brendamichele16@hotmail.com>
2022-11-30 07:38:50 -0300

    Teste

---
+++ org.leetcode/src/main/java/org/leetcode/weekOne/ValidPalindrome.java
@@ -0,0 +1,15 @@
+package org.leetcode.weekOne;
+
+public class ValidPalindrome {
+
+    public static void main(String[] args) {
+
+        System.out.println("It is a palindrome: " + isPalindrome("aça"));
+    }
+
+    public static boolean isPalindrome(String s) {
+
+
+        return true;
+    }
+}
