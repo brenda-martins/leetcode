0c7a3e016a7a86145f65185bb4515bf0f6d2ef66
Brenda Martins <brendamichele16@hotmail.com>
2022-11-30 21:54:13 -0300

    Implementing second solution to ValidPalindrome

--- org.leetcode/src/main/java/org/leetcode/weekOne/ValidPalindrome.java
+++ org.leetcode/src/main/java/org/leetcode/weekOne/ValidPalindrome.java
@@ -5,16 +5,18 @@ import java.util.Stack;
 public class ValidPalindrome {
 
     public static void main(String[] args) {
-
-        System.out.println("It is a palindrome: " + isPalindrome("ada"));
+        System.out.println("[First solution] It is a palindrome: " + isPalindrome("A man, a plan, a canal: Panama"));
+        System.out.println("[Second solution] It is a palindrome: " + isPalindromeSolutionTwo("A mana, a plan, a canal: Panama"));
     }
 
     public static boolean isPalindrome(String s) {
+
+        final var string = removeNonAlphanumeric(s).toLowerCase();
+
         Stack<Character> stack = new Stack<>();
 
-        for (int i = 0; i <  s.length(); i++) {
-
-            stack.push(s.charAt(i));
+        for (int i = 0; i <  string.length(); i++) {
+            stack.push(string.charAt(i));
         }
 
         String palindrome = "";
@@ -23,10 +25,27 @@ public class ValidPalindrome {
             palindrome += stack.pop();
         }
 
-        if(palindrome.equalsIgnoreCase(s)){
+        if(palindrome.equalsIgnoreCase(string)){
             return true;
         }
-
         return false;
+    }
+
+
+    public static boolean isPalindromeSolutionTwo(String s) {
+        final var string = removeNonAlphanumeric(s).toLowerCase();
+
+        var l = 0;
+        var r = string.length() - 1;
+
+        while(l < r){
+            if (string.charAt(l) != string.charAt(r))  return false;
+            l++;
+            r--;
+        }
+        return true;
+    }
+    private static String removeNonAlphanumeric(String str) {
+        return str.replaceAll("[^a-zA-Z0-9]", "");
     }
 }
