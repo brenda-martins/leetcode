a9753a7f17b63f6aa5a3b6af09f94bc86fbd77d1
Brenda Martins <brendamichele16@hotmail.com>
2022-12-02 17:49:37 -0300

    Resolved ValidAnagram problem

---
+++ org.leetcode/src/main/java/org/leetcode/weekOne/ValidAnagram.java
@@ -0,0 +1,47 @@
+package org.leetcode.weekOne;
+import java.util.HashMap;
+
+//Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+//
+//An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase,
+// typically using all the original letters exactly once.
+
+
+public class ValidAnagram {
+
+    public static void main(String[] args) {
+
+        var s = "anagram";
+        var t = "nagaram";
+
+        System.out.println("Is an anagram? " + isAnagram(s, t));
+    }
+
+    public static boolean isAnagram(String s, String t) {
+
+        if(s.length() != t.length()) return  false;
+
+        var sMap = new HashMap<Character, Integer>();
+        var tMap = new HashMap<Character, Integer>();
+
+        for (int i = 0; i < s.length(); i++){
+
+            var sChar = s.charAt(i);
+            var sTotal = sMap.get(sChar) != null ? sMap.get(sChar) + 1 : 1 ;
+
+            var tChar = t.charAt(i);
+            var tTotal = tMap.get(tChar) != null ? tMap.get(tChar) + 1 : 1;
+
+            sMap.put(sChar, sTotal);
+            tMap.put(tChar, tTotal);
+        }
+
+      for (Character c : sMap.keySet()){
+          if(!sMap.get(c).equals(tMap.get(c))){
+              return  false;
+          }
+      }
+
+        return true;
+    }
+}
