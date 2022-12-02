574385aaae7d5d83db73b9806d72fca888e0b4f8
Brenda Martins <brendamichele16@hotmail.com>
2022-12-02 16:31:07 -0300

    Addeding a description

--- org.leetcode/src/main/java/org/leetcode/weekOne/InvertBinaryTree.java
+++ org.leetcode/src/main/java/org/leetcode/weekOne/InvertBinaryTree.java
@@ -1,5 +1,16 @@
 package org.leetcode.weekOne;
 
+//Given the root of a binary tree, invert the tree, and return its root.
+//
+//           4              4
+//          / \            / \
+//         2   7          7   2
+//        /\   /\        /\   /\
+//        1 3 6  9     9  6   3 1
+
+//Input: root = [4,2,7,1,3,6,9]
+//Output: [4,7,2,9,6,3,1]
+
 public class InvertBinaryTree {
 
     public static TreeNode invertTree(TreeNode root) {
