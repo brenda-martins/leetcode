a8e4fe6e91d2d03c0b68b897ffc6ea393a53d03e
Brenda Martins <brendamichele16@hotmail.com>
2022-12-26 20:44:29 -0300

    Resolved LinkedListCycle problem

--- org.leetcode/src/main/java/org/leetcode/weekOne/LinkedListCycle.java
+++ org.leetcode/src/main/java/org/leetcode/weekOne/LinkedListCycle.java
@@ -1,2 +1,27 @@
-package org.leetcode.weekOne;public class LinkedListCycle {
+package org.leetcode.weekOne;
+
+public class LinkedListCycle {
+
+
+    public boolean hasCycle(ListNode head) {
+        var s = head;
+        var f = head;
+
+        while(f != null && f.next != null){
+            s = s.next;
+            f = f.next.next;
+
+            if(s == f) return true;
+        }
+        return false;
+    }
+
+    class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) {
+          val = x;
+          next = null;
+      }
+  }
 }
