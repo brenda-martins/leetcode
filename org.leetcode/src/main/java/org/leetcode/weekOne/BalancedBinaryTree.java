5ade68a263d0dda75cf53a032a03955c9962704a
Brenda Martins <brendamichele16@hotmail.com>
2022-12-26 19:56:40 -0300

    Including coments

--- org.leetcode/src/main/java/org/leetcode/weekOne/BalancedBinaryTree.java
+++ org.leetcode/src/main/java/org/leetcode/weekOne/BalancedBinaryTree.java
@@ -1,5 +1,9 @@
 package org.leetcode.weekOne;
 
+//Given a binary tree, determine if it is  height-balanced
+//
+//        A height-balanced binary tree is a binary tree in which the
+//        depth of the two subtrees of every node never differs by more than one.
 public class BalancedBinaryTree {
 
      public class TreeNode {
